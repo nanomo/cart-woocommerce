2fdec6091a76304a4af520fc24e99c2e7b833fc1
Giovanni Cavallari <jobacavallari@hotmail.com>
2020-11-18 18:40:23 -0300

    Minify review.js

--- assets/js/review.js
+++ assets/js/review.js
@@ -1,5 +1,6 @@
+/* globals ajaxurl */
 jQuery(document).ready(function ($) {
-  $(document).on("click", ".mp-rating-notice button", function () {
+  $(document).on('click', '.mp-rating-notice button', function () {
       $.post( ajaxurl, { action: 'mercadopago_review_dismiss' } );
     }
   );
