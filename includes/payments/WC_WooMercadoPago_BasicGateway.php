d2ae1eec218fdcef3f40e88703ea1eb1992e33e0
Fabio Landi <fabio.landi@mercadolivre.com>
2020-12-29 18:34:18 -0300

    Changed name of checkout for Checkout Pro

--- includes/payments/WC_WooMercadoPago_BasicGateway.php
+++ includes/payments/WC_WooMercadoPago_BasicGateway.php
@@ -27,7 +27,7 @@ class WC_WooMercadoPago_BasicGateway extends WC_WooMercadoPago_PaymentAbstract
         $this->description = __('It offers all means of payment: credit and debit cards, cash and account money. Your customers choose whether they pay as guests or from their Mercado Pago account.', 'woocommerce-mercadopago');
 
         $this->form_fields = array();
-        $this->method_title = __('Mercado Pago Checkout', 'woocommerce-mercadopago');
+        $this->method_title = __('Checkout Pro', 'woocommerce-mercadopago');
         $this->method = $this->getOption('method', 'redirect');
         $this->title = __('Pay with the payment method you prefer', 'woocommerce-mercadopago');
         $this->method_description = $this->getMethodDescription($this->description);
@@ -243,7 +243,7 @@ class WC_WooMercadoPago_BasicGateway extends WC_WooMercadoPago_PaymentAbstract
     {
         $checkout_header = array(
             'title' => sprintf(
-                __('Mercado Pago checkout %s', 'woocommerce-mercadopago'),
+                __('Checkout Pro %s', 'woocommerce-mercadopago'),
                 '<div class="row">
                 <div class="mp-col-md-12 mp_subtitle_header">
                 ' . __('Accept all method of payment and take your charges to another level', 'woocommerce-mercadopago') . '
@@ -280,7 +280,7 @@ class WC_WooMercadoPago_BasicGateway extends WC_WooMercadoPago_PaymentAbstract
     public function field_checkout_options_description()
     {
         $checkout_options_description = array(
-            'title' => __('Enable the experience of the Mercado Pago Checkout in your online store, select the means of payment available to your customers and<br> define the maximum fees in which they can pay you.', 'woocommerce-mercadopago'),
+            'title' => __('Enable the experience of the Checkout Pro in your online store, select the means of payment available to your customers and<br> define the maximum fees in which they can pay you.', 'woocommerce-mercadopago'),
             'type' => 'title',
             'class' => 'mp_small_text'
         );
@@ -613,7 +613,7 @@ class WC_WooMercadoPago_BasicGateway extends WC_WooMercadoPago_PaymentAbstract
                 'redirect' => $this->create_preference($order)
             );
         } elseif ('modal' == $this->method) {
-            $this->log->write_log(__FUNCTION__, 'preparing to render Mercado Pago checkout view.');
+            $this->log->write_log(__FUNCTION__, 'preparing to render Checkout Pro view.');
             return array(
                 'result' => 'success',
                 'redirect' => $order->get_checkout_payment_url(true)
