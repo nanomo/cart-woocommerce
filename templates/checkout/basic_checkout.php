c02c953b3312f8ded1bb9a4057c3c4bd2d202088
Fernando Lira <fernando.lira@mercadolivre.com>
2021-01-07 16:30:10 -0300

    Add file headers

--- templates/checkout/basic_checkout.php
+++ templates/checkout/basic_checkout.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Part of Woo Mercado Pago Module
+ * Author - Mercado Pago
+ * Developer
+ * Copyright - Copyright(c) MercadoPago [https://www.mercadopago.com]
+ * License - https://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ *
+ * @package MercadoPago
+ * @category Template
+ * @author Mercado Pago
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
