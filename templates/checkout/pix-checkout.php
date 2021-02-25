6922635ac461380c70874a656178bfddca14bce4
katelucena-meli <kate.lucena@mercadolivre.com>
2021-02-25 18:11:46 -0300

    Added image and adjustments to Payment_fields at checkout_pix

--- templates/checkout/pix-checkout.php
+++ templates/checkout/pix-checkout.php
@@ -18,5 +18,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <div class="mp-panel-checkout">
 	<div class="mp-row-checkout">
+	<div class="mp-redirect-frame">
+			<img src="<?php echo esc_html( $image_pix ); ?>" class="mp-img-fluid mp-img-redirect" alt=""/>
+			<p><?php echo esc_html_e( 'Pague de forma segura e instantânea! Ao finalizar o pedido, você verá o código para fazer o pagamento.', 'woocommerce-mercadopago' ); ?></p>
+		</div>
 	</div>
 </div>
