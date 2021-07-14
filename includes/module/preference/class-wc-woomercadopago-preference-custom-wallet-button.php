a06133b522b595e664d392dd616c8fa6ed020607
Fernando Lira <fernando.lira@mercadolivre.com>
2021-07-14 18:12:43 -0300

    feat(wallet button): fixed preference to wallet button

--- includes/module/preference/class-wc-woomercadopago-preference-custom-wallet-button.php
+++ includes/module/preference/class-wc-woomercadopago-preference-custom-wallet-button.php
@@ -18,4 +18,27 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class WC_WooMercadoPago_Preference_Custom_Wallet_Button extends WC_WooMercadoPago_Preference_Basic {
 
+	/**
+	 * WC_WooMercadoPago_Preference_Custom_Wallet_Button constructor.
+	 *
+	 * @param $payment
+	 * @param $order
+	 */
+	public function __construct( $payment, $order ) {
+		parent::__construct( $payment, $order );
+		$this->preference['purpose'] = 'wallet_purchase';
+	}
+
+	/**
+	 * Overwrite the default method to set Wallet Button Data
+	 *
+	 * @return string[]
+	 */
+	public function get_internal_metadata_basic() {
+		return array(
+			'checkout'      => 'pro',
+			'checkout_type' => 'wallet_button',
+		);
+	}
+
 }
