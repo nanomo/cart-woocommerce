1ead76bb43c7da002f6092c91c8b13c5d3695f45
Luana Tammy Uchida Ogawa <luana.ogawa@mercadolivre.com>
2021-08-09 12:21:03 -0300

    modifying basic gateway credentials order

--- assets/js/basic_config_mercadopago.js
+++ assets/js/basic_config_mercadopago.js
@@ -50,8 +50,8 @@ window.addEventListener('load', function() {
 
   //clone save button
   var cloneSaveButton = document.getElementById('woocommerce_woo-mercado-pago-basic_checkout_btn_save');
-  if (document.getElementById('woocommerce_woo-mercado-pago-basic_checkout_homolog_title') !== null || document.getElementById('woocommerce_woo-mercado-pago-basic_checkout_credential_description_prod') !== null) {
-    document.getElementById('woocommerce_woo-mercado-pago-basic_checkout_credential_description_prod').nextElementSibling.append(cloneSaveButton.cloneNode(true));
+  if (document.getElementById('woocommerce_woo-mercado-pago-basic_checkout_homolog_title') !== null || document.getElementById('woocommerce_woo-mercado-pago-basic_checkout_credential_description_test') !== null) {
+    document.getElementById('woocommerce_woo-mercado-pago-basic_checkout_credential_description_test').nextElementSibling.append(cloneSaveButton.cloneNode(true));
   }
 
   if (document.getElementById('woocommerce_woo-mercado-pago-basic_checkout_homolog_title') !== null || document.getElementById('woocommerce_woo-mercado-pago-basic_checkout_options_title') !== null) {
