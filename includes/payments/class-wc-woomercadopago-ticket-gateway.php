3bd5dbfd5a6bc88cbaa55afcfad316311a4bbb75
SalehKhalilMP <saleh.khalil@mercadolivre.com>
2021-08-24 15:58:31 -0300

    feat(admin-checkout-mode-alert): Include alert on payment gateways

--- includes/payments/class-wc-woomercadopago-ticket-gateway.php
+++ includes/payments/class-wc-woomercadopago-ticket-gateway.php
@@ -124,6 +124,7 @@ class WC_WooMercadoPago_Ticket_Gateway extends WC_WooMercadoPago_Payment_Abstrac
 			'checkout_subtitle_checkout_mode',
 			'checkbox_checkout_test_mode',
 			'checkbox_checkout_production_mode',
+			'checkout_mode_alert',
 			'checkout_credential_link',
 			'checkout_credential_title_prod',
 			'checkout_credential_description_prod',
