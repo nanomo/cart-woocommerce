fe4aed23221be325c70196fca47ab9ab1395887e
cleitonaguiarandrade <cleiton.andrade@mercadolivre.com>
2021-10-15 13:22:54 -0300

    created function to get access token and payment id

--- includes/pix/class-wc-woomercadopago-image-generator.php
+++ includes/pix/class-wc-woomercadopago-image-generator.php
@@ -41,6 +41,32 @@ class WC_WooMercadoPago_Image_Generator{
     echo "testing endpoint";
 
   }  
+
+  public static function get_access_data(){
+    
+    $post = $_GET['id'];
+    $order = wc_get_order($post);
+    $payment_method                = $order->get_payment_method();
+		$is_mercadopago_payment_method = in_array($payment_method, WC_WooMercadoPago_Constants::GATEWAYS_IDS, true);
+		$payment_ids                   = explode(',', $order->get_meta( '_Mercado_Pago_Payment_IDs' ));
+
+    
+		if ( ! $is_mercadopago_payment_method || empty($payment_ids) ) {
+			return;
+		}
+    
+		$is_production_mode = $order->get_meta( 'is_production_mode' );
+		$access_token       = 'no' === $is_production_mode || ! $is_production_mode
+			? get_option( '_mp_access_token_test' )
+			: get_option( '_mp_access_token_prod' );
+    
+    $data = array(
+			'payment_id'     => $payment_ids,
+			'access_token' => $access_token,
+		);
+    
+    return $data;
+	}
   
 	/**
 	 * Init Mercado Pago Image Generator Class
