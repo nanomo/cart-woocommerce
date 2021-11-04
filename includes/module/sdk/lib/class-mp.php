2123c21f7ea3414a740df8f1d3d29fce5d5082c2
cleitonaguiarandrade <cleiton.andrade@mercadolivre.com>
2021-11-04 10:58:28 -0300

    Indentation fixes

--- includes/module/sdk/lib/class-mp.php
+++ includes/module/sdk/lib/class-mp.php
@@ -618,13 +618,13 @@ class MP {
 
 		asort( $response );
 
-    foreach ( $response['response'] as $key => $value ) {	
-			$response['response'][$key]['payment_places'] =  $this->build_payment_places($value['id']); 
-			
-    }
-    
+		foreach ( $response['response'] as $key => $value ) {
+			$response['response'][$key]['payment_places'] = $this->build_payment_places($value['id']);
+
+		}
+
 		$this->set_cache_response($key, $response);
-   
+
 		return $response;
 	}
 
@@ -845,54 +845,54 @@ class MP {
 			$log->write_log( $function, $message );
 		}
 	}
-  
+
 	/**
 	 * Buil array payment places
 	 *
 	 * @param $lpayment_id
 	 */
-	public function build_payment_places($payment_id) {
+	public function build_payment_places( $payment_id ) {
 
 		$payment_places =
-		[  
-      'paycash' => [
-                    [
-                      'payment_option_id' => '7eleven',
-                      'name' => '7 Eleven',
-                      'status' => 'active',
-                      'thumbnail' => 'https://http2.mlstatic.com/storage/logos-api-admin/417ddb90-34ab-11e9-b8b8-15cad73057aa-s.png'
-                    ],
-                    [
-                      'payment_option_id' => 'circlek',
-                      'name' => 'Circle K',
-                      'status' => 'active',
-                      'thumbnail' => 'https://http2.mlstatic.com/storage/logos-api-admin/6f952c90-34ab-11e9-8357-f13e9b392369-s.png'
-                    ],
-                    [
-                      'payment_option_id' => 'soriana',
-                      'name' => 'Soriana',
-                      'status' => 'active',
-                      'thumbnail' => 'https://http2.mlstatic.com/storage/logos-api-admin/dac0bf10-01eb-11ec-ad92-052532916206-s.png'
-                    ],
-                    [
-                      'payment_option_id' => 'extra',
-                      'name' => 'Extra',
-                      'status' => 'active',
-                      'thumbnail' => 'https://http2.mlstatic.com/storage/logos-api-admin/9c8f26b0-34ab-11e9-b8b8-15cad73057aa-s.png'
-                    ],
-                    [
-                      'payment_option_id' => 'calimax',
-                      'name' => 'Calimax',
-                      'status' => 'active',
-                      'thumbnail' => 'https://http2.mlstatic.com/storage/logos-api-admin/52efa730-01ec-11ec-ba6b-c5f27048193b-s.png'
-                    ],
-    ],
-	] ;
-    
-    if (array_key_exists($payment_id, $payment_places)) {
-            return $payment_places[$payment_id];
-  }
-    
+		[
+			'paycash' => [
+						[
+						'payment_option_id' => '7eleven',
+						'name' => '7 Eleven',
+						'status' => 'active',
+						'thumbnail' => 'https://http2.mlstatic.com/storage/logos-api-admin/417ddb90-34ab-11e9-b8b8-15cad73057aa-s.png'
+						],
+						[
+						'payment_option_id' => 'circlek',
+						'name' => 'Circle K',
+						'status' => 'active',
+						'thumbnail' => 'https://http2.mlstatic.com/storage/logos-api-admin/6f952c90-34ab-11e9-8357-f13e9b392369-s.png'
+						],
+						[
+						'payment_option_id' => 'soriana',
+						'name' => 'Soriana',
+						'status' => 'active',
+						'thumbnail' => 'https://http2.mlstatic.com/storage/logos-api-admin/dac0bf10-01eb-11ec-ad92-052532916206-s.png'
+						],
+						[
+						'payment_option_id' => 'extra',
+						'name' => 'Extra',
+						'status' => 'active',
+						'thumbnail' => 'https://http2.mlstatic.com/storage/logos-api-admin/9c8f26b0-34ab-11e9-b8b8-15cad73057aa-s.png'
+						],
+						[
+						'payment_option_id' => 'calimax',
+						'name' => 'Calimax',
+						'status' => 'active',
+						'thumbnail' => 'https://http2.mlstatic.com/storage/logos-api-admin/52efa730-01ec-11ec-ba6b-c5f27048193b-s.png'
+						],
+	],
+	];
+
+		if ( array_key_exists($payment_id, $payment_places) ) {
+			return $payment_places[$payment_id];
+		}
+
 		return [];
 	}
 
