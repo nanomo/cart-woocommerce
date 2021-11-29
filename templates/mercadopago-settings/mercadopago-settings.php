fb5b780e1ad9b4c5cc7582cda9ef35045203784e
cleitonaguiarandrade <cleiton.andrade@mercadolivre.com>
2021-11-29 19:34:11 -0300

    Links and requirements block

--- templates/mercadopago-settings/mercadopago-settings.php
+++ templates/mercadopago-settings/mercadopago-settings.php
@@ -10,14 +10,70 @@
  * @package MercadoPago
  */
 
-if ( ! defined('ABSPATH') ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 ?>
 
 <div class="mp-settings">
-<div class="mp-settings-header"> 
-	<img src="<?php echo esc_url( plugins_url( '../assets/images/mercadopago-settings/header-settings.png', plugin_dir_path( __FILE__ ) ) ); ?>">
-	<p>Aceite <b>pagamentos no ato<b> com toda a <b>segurança<b> Mercado Pago</p>
+	<div class="mp-settings-header">
+		<img src="<?php echo esc_url(plugins_url('../assets/images/mercadopago-settings/header-settings.png', plugin_dir_path(__FILE__))); ?>">
+		<p>Aceite <b>pagamentos no ato<b> com toda a <b>segurança<b> Mercado Pago</p>
+	</div>
 </div>
+<div class="mp-container">
+	<div class="mp-block">
+		<h2>Requisitos técnicos</h2>
+		<div class="mp-inner-container">
+			<div>
+				<h4>SSL</h4>
+				<img class="mp-icon" src="<?php echo esc_url(plugins_url('../assets/images/mercadopago-settings/icon_shape.png', plugin_dir_path(__FILE__))); ?>">
+			</div>
+			<div>
+				<img class="mp-credential-input-success">
+			</div>
+		</div>
+		<hr>
+		<div class="mp-inner-container">
+			<div>
+				<h4>Extensões GD</h4>
+				<img class="mp-icon" src="<?php echo esc_url(plugins_url('../assets/images/mercadopago-settings/icon_shape.png', plugin_dir_path(__FILE__))); ?>">
+			</div>
+			<div>
+				<img class="mp-credential-input-success">
+			</div>
+		</div>
+		<hr>
+		<div class="mp-inner-container">
+			<div>
+				<h4>Curl</h4>
+				<img class="mp-icon" src="<?php echo esc_url(plugins_url('../assets/images/mercadopago-settings/icon_shape.png', plugin_dir_path(__FILE__))); ?>">
+			</div>
+			<div>
+				<img class="mp-credential-input-success">
+			</div>
+		</div>
+	</div>
+	<div class="mp-block">
+		<div class="mp-inner-container-settings">
+			<div>
+				<h2>Recebimentos e parcelamento</h2>
+				<p>Escolha <span>quando quer receber o dinheiro</span> das vendas e se quer oferecer <span>parcelamento sem juros</span> aos clientes. </p>
+			</div>
+			<div>
+				<button class="mp-button">Ajustar prazos e taxas </button>
+			</div>
+		</div>
+	</div>
+	<div class="mp-block">
+		<div class="mp-inner-container-settings">
+			<div>
+				<h2>Dúvidas?</h2>
+				<p>Revise o passo a passo de <span>como integrar o Plugin do Mercado Pago</span> no nosso site de desenvolvedores. </p>
+			</div>
+			<div>
+				<button class="mp-button"> Manual do plugin </button>
+			</div>
+		</div>
+	</div>
 </div>
