f61140c458a1477d5266d9a2294709ebc14b90b9
Giovanni Cavallari <giovanni.cavallari@mercadolivre.com>
2021-12-13 11:31:18 -0300

    feat: updated cho pro

--- templates/checkout/basic-checkout.php
+++ templates/checkout/basic-checkout.php
@@ -19,38 +19,38 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<?php if ( true === $test_mode ) : ?>
 			<div class="mp-checkout-pro-test-mode">
 				<test-mode
-					title="<?= esc_html_e( 'Checkout Pro em Modo Teste', 'woocommerce-mercadopago' ); ?>"
-					description="<?= esc_html_e( 'Utilize meios do Mercado Pago sem cobranças reais. ', 'woocommerce-mercadopago' ); ?>"
-					linkText="<?= esc_html_e( 'Consulte as regras do modo teste.', 'woocommerce-mercadopago' ); ?>"
-					linkUrl="<?= esc_html( $test_mode_link )  ?>"
+					title="<? echo esc_html_e( 'Checkout Pro em Modo Teste', 'woocommerce-mercadopago' ); ?>"
+					description="<? echo esc_html_e( 'Utilize meios do Mercado Pago sem cobranças reais. ', 'woocommerce-mercadopago' ); ?>"
+					linkText="<? echo esc_html_e( 'Consulte as regras do modo teste.', 'woocommerce-mercadopago' ); ?>"
+					linkUrl="<? echo esc_html( $test_mode_link )  ?>"
 				>
 				</test-mode>
 			</div>
 		<?php endif; ?>
 
 		<checkout-benefits
-			title="<?= esc_html_e( 'Paga más rápido con Mercado Pago', 'woocommerce-mercadopago' ); ?>"
+			title="<? echo esc_html_e( 'Paga más rápido con Mercado Pago', 'woocommerce-mercadopago' ); ?>"
 			items='[
-				"<?= esc_html_e( 'Pago seguro', 'woocommerce-mercadopago' ); ?>",
-				"<?= esc_html_e( 'Sin cargar datos', 'woocommerce-mercadopago' ); ?>",
-				"<?= esc_html_e( 'Cuotas disponibles', 'woocommerce-mercadopago' ); ?>"
+				"<? echo esc_html_e( 'Pago seguro', 'woocommerce-mercadopago' ); ?>",
+				"<? echo esc_html_e( 'Sin cargar datos', 'woocommerce-mercadopago' ); ?>",
+				"<? echo esc_html_e( 'Cuotas disponibles', 'woocommerce-mercadopago' ); ?>"
 			]'
-			list-style-type-src="<?= esc_html( $list_style_type_src ) ?>"
-			list-style-type-alt="<?= esc_html_e( 'List style type blue check', 'woocommerce-mercadopago' ); ?>"
+			list-style-type-src="<? echo esc_html( $list_style_type_src ) ?>"
+			list-style-type-alt="<? echo esc_html_e( 'List style type blue check', 'woocommerce-mercadopago' ); ?>"
 		>
 		</checkout-benefits>
 
 		<div class="mp-checkout-pro-payment-methods">
-			<payment-methods methods="<?= esc_html( $payment_methods );  ?>"></payment-methods>
+			<payment-methods methods="<? echo esc_html( $payment_methods );  ?>"></payment-methods>
 		</div>
 	</div>
 
 	<?php if ( 'redirect' === $method ) : ?>
 		<div class="mp-checkout-pro-redirect">
 			<checkout-redirect
-				text="<?= esc_html_e( 'Al confirmar tu compra, te redirigiremos a tu cuenta de Mercado Pago', 'woocommerce-mercadopago' ); ?>"
-				src="<?= esc_html( $redirect_image ); ?>"
-				alt="<?= esc_html_e( 'Checkout Pro redirect info image', 'woocommerce-mercadopago' ); ?>"
+				text="<? echo esc_html_e( 'Al confirmar tu compra, te redirigiremos a tu cuenta de Mercado Pago', 'woocommerce-mercadopago' ); ?>"
+				alt="<? echo esc_html_e( 'Checkout Pro redirect info image', 'woocommerce-mercadopago' ); ?>"
+				src="<? echo esc_html( $redirect_image ); ?>"
 			>
 			</checkout-redirect>
 		</div>
@@ -59,5 +59,5 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <!-- Terms and conditions link at checkout -->
 <div class="mp-checkout-pro-terms-and-conditions">
-	<terms-and-conditions href="<?= esc_html( $link_terms_and_conditions ) ?>"></terms-and-conditions>
+	<terms-and-conditions href="<? echo esc_html( $link_terms_and_conditions ) ?>"></terms-and-conditions>
 </div>
