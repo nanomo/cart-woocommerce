e68b78e7785e9f991275989750ca2f7c23223c72
katelucena-meli <kate.lucena@mercadolivre.com>
2021-12-13 18:43:28 -0300

    Added option class in init

--- includes/module/class-wc-woomercadopago-init.php
+++ includes/module/class-wc-woomercadopago-init.php
@@ -189,6 +189,7 @@ class WC_WooMercadoPago_Init {
 			require_once dirname( __FILE__ ) . '../../admin/notices/class-wc-woomercadopago-review-notice.php';
 			require_once dirname( __FILE__ ) . '../../pix/class-wc-woomercadopago-image-generator.php';
 			require_once dirname( __FILE__ ) . '/mercadopago-settings/class-wc-woomercadopago-mercadopago-settings.php';
+			require_once dirname( __FILE__ ) . '/mercadopago-settings/class-wc-woomercadopago-options.php';
 
 			WC_WooMercadoPago_Module::init_mercado_pago_class();
 			WC_WooMercadoPago_Review_Notice::init_mercadopago_review_notice();
