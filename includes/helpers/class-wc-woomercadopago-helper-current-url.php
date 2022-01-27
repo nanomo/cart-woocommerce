4ccb02b3bf9d28cc1dce33f98a958367e0af913f
Gustavo Brito <gustavo.brito@mercadolivre.com>
2022-01-27 17:08:50 -0300

    changing identation to tabs

--- includes/helpers/class-wc-woomercadopago-helper-current-url.php
+++ includes/helpers/class-wc-woomercadopago-helper-current-url.php
@@ -18,42 +18,42 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class WC_WooMercadoPago_Helper_Current_Url {
 
-    public static function get_current_page() {
+	public static function get_current_page() {
 		// @codingStandardsIgnoreLine
 		$current_page = isset( $_GET['page'] ) ? sanitize_text_field( $_GET['page'] ) : '';
 
-        return $current_page;
-    }
+		return $current_page;
+	}
 
-    public static function get_current_section() {
+	public static function get_current_section() {
 		// @codingStandardsIgnoreLine
 		$current_section = isset( $_GET['section'] ) ? sanitize_text_field( $_GET['section'] ) : '';
 
-        return $current_section;
-    }
+		return $current_section;
+	}
 
-    public static function validate_page( $expected_page, $current_page = null, $allow_partial_match = false ) {
-        if ( ! $current_page ) {
-            $current_page = WC_WooMercadoPago_Helper_Current_Url::get_current_page();
-        }
+	public static function validate_page( $expected_page, $current_page = null, $allow_partial_match = false ) {
+		if ( ! $current_page ) {
+			$current_page = self::get_current_page();
+		}
 
-        return WC_WooMercadoPago_Helper_Current_Url::compare_strings( $expected_page, $current_page );
-    }
+		return self::compare_strings( $expected_page, $current_page );
+	}
 
-    public static function validate_session( $expected_section, $current_section = null, $allow_partial_match = true ) {
-        if ( ! $expected_section ) {
-            $current_section = WC_WooMercadoPago_Helper_Current_Url::get_current_section();
-        }
+	public static function validate_session( $expected_section, $current_section = null, $allow_partial_match = true ) {
+		if ( ! $expected_section ) {
+			$current_section = self::get_current_section();
+		}
 
-        return WC_WooMercadoPago_Helper_Current_Url::compare_strings( $expected_section, $current_section );
-    }
+		return self::compare_strings( $expected_section, $current_section );
+	}
 
-    public static function compare_strings( $expected, $current, $allow_partial_match ) {
-        if ( $allow_partial_match ) {
-            return strpos($expected, $current) !== false;
-        }
-        
-        return $expected === $current;
-    }
+	public static function compare_strings( $expected, $current, $allow_partial_match ) {
+		if ( $allow_partial_match ) {
+			return strpos($expected, $current) !== false;
+		}
+
+		return $expected === $current;
+	}
 
 }
