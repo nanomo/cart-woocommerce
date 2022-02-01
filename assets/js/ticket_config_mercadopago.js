25b1c294d44f129c0a6942f9b347ea56298697b6
katelucena-meli <kate.lucena@mercadolivre.com>
2022-02-01 18:44:17 -0300

    Deleted update checkout mode method and tables method in js

--- assets/js/ticket_config_mercadopago.js
+++ assets/js/ticket_config_mercadopago.js
@@ -10,7 +10,6 @@ const payment_mercado_pago_ticket = {
     this.setTitleDescriptionStyle();
     this.setHide();
     this.makeCollapsibleAdvancedConfig();
-    this.setTableIds();
     this.createOfflinePaymentsCheckAll();
   },
   hasConfigurations: function () {
@@ -24,88 +23,86 @@ const payment_mercado_pago_ticket = {
     settings_table.nextElementSibling.remove();
   },
   setHide: function () {
-    document.querySelector('.wc-admin-breadcrumb').style.display = 'none';
-    if (document.querySelector('.mp-header-logo') !== null) {
-      document.querySelector('.mp-header-logo').style.display = 'none';
+    document.querySelector(".wc-admin-breadcrumb").style.display = "none";
+    if (document.querySelector(".mp-header-logo") !== null) {
+      document.querySelector(".mp-header-logo").style.display = "none";
     } else {
-      var pElement = document.querySelectorAll('#mainform > p');
-      pElement[0] !== undefined ? pElement[0].style.display = 'none' : null;
+      var pElement = document.querySelectorAll("#mainform > p");
+      pElement[0] !== undefined ? (pElement[0].style.display = "none") : null;
     }
 
-    var h2s = document.querySelectorAll('h2');
-    h2s[4] !== undefined ? h2s[4].style.display = 'none' : null;
+    var h2s = document.querySelectorAll("h2");
+    h2s[4] !== undefined ? (h2s[4].style.display = "none") : null;
 
-    document.querySelectorAll('.hidden-field-mp-title').forEach(
-      (element) => {
-        element.closest('tr').style.display = 'none';
-      }
-    );
+    document.querySelectorAll(".hidden-field-mp-title").forEach((element) => {
+      element.closest("tr").style.display = "none";
+    });
 
-    document.querySelectorAll('.hidden-field-mp-desc').forEach(
-      (element) => {
-        element.closest('tr').style.display = 'none';
-      }
-    );
+    document.querySelectorAll(".hidden-field-mp-desc").forEach((element) => {
+      element.closest("tr").style.display = "none";
+    });
   },
   setInputMaxLength: function () {
-    let titleInput = document.querySelectorAll('.limit-title-max-length');
+    let titleInput = document.querySelectorAll(".limit-title-max-length");
 
-    titleInput.forEach(
-      (element) => {
-        element.setAttribute('maxlength', '85');
-      }
-    );
+    titleInput.forEach((element) => {
+      element.setAttribute("maxlength", "85");
+    });
   },
   setTitleDescriptionStyle: function () {
     //update form_fields label
-    var label = document.querySelectorAll('th.titledesc');
+    var label = document.querySelectorAll("th.titledesc");
     for (var j = 0; j < label.length; j++) {
-      label[j].id = 'mp_field_text';
-      if (label[j] && label[j].children[0] && label[j].children[0].children[0]) {
-        label[j].children[0].children[0].style.position = 'relative';
-        label[j].children[0].children[0].style.fontSize = '22px';
+      label[j].id = "mp_field_text";
+      if (
+        label[j] &&
+        label[j].children[0] &&
+        label[j].children[0].children[0]
+      ) {
+        label[j].children[0].children[0].style.position = "relative";
+        label[j].children[0].children[0].style.fontSize = "22px";
       }
-    }
-  },
-  setTableIds: function () {
-    var tablePayments = document.querySelector('#woocommerce_woo-mercado-pago-pix_checkout_pix_payments_description').nextElementSibling.getAttribute('class');
-    var mp_input_payments = document.querySelectorAll('.' + tablePayments + ' td.forminp label');
-    for (var ip = 0; ip < mp_input_payments.length; ip++) {
-      mp_input_payments[ip].id = 'mp_input_payments_mt';
     }
   },
   makeCollapsibleAdvancedConfig: function () {
     //collpase Configuración Avanzada
-    var collapse_title_2 = document.querySelector('#woocommerce_woo-mercado-pago-ticket_checkout_ticket_payments_advanced_title');
-    var collapse_table_2 = document.querySelector('#woocommerce_woo-mercado-pago-ticket_checkout_payments_advanced_description').nextElementSibling;
-    var collapse_description_2 = document.querySelector('#woocommerce_woo-mercado-pago-ticket_checkout_payments_advanced_description');
-    collapse_table_2.style.display = 'none';
-    collapse_description_2.style.display = 'none';
-    collapse_title_2.style.cursor = 'pointer';
+    var collapse_title_2 = document.querySelector(
+      "#woocommerce_woo-mercado-pago-ticket_checkout_ticket_payments_advanced_title"
+    );
+    var collapse_table_2 = document.querySelector(
+      "#woocommerce_woo-mercado-pago-ticket_checkout_payments_advanced_description"
+    ).nextElementSibling;
+    var collapse_description_2 = document.querySelector(
+      "#woocommerce_woo-mercado-pago-ticket_checkout_payments_advanced_description"
+    );
+    collapse_table_2.style.display = "none";
+    collapse_description_2.style.display = "none";
+    collapse_title_2.style.cursor = "pointer";
 
-    collapse_title_2.innerHTML += '<span class="mp-btn-collapsible" id="header_plus_2" style="display:block">+</span>\
+    collapse_title_2.innerHTML +=
+      '<span class="mp-btn-collapsible" id="header_plus_2" style="display:block">+</span>\
       <span class="mp-btn-collapsible" id="header_less_2" style="display:none">-</span>';
 
-    var header_plus_2 = document.querySelector('#header_plus_2');
-    var header_less_2 = document.querySelector('#header_less_2');
+    var header_plus_2 = document.querySelector("#header_plus_2");
+    var header_less_2 = document.querySelector("#header_less_2");
 
     collapse_title_2.onclick = function () {
-      if (collapse_table_2.style.display === 'none') {
-        collapse_table_2.style.display = 'block';
-        collapse_description_2.style.display = 'block';
-        header_less_2.style.display = 'block';
-        header_plus_2.style.display = 'none';
+      if (collapse_table_2.style.display === "none") {
+        collapse_table_2.style.display = "block";
+        collapse_description_2.style.display = "block";
+        header_less_2.style.display = "block";
+        header_plus_2.style.display = "none";
       } else {
-        collapse_table_2.style.display = 'none';
-        collapse_description_2.style.display = 'none';
-        header_less_2.style.display = 'none';
-        header_plus_2.style.display = 'block';
+        collapse_table_2.style.display = "none";
+        collapse_description_2.style.display = "none";
+        header_less_2.style.display = "none";
+        header_plus_2.style.display = "block";
       }
     };
   },
   completeOfflineCheckbox: function () {
-    var offlineCheck = document.getElementById('checkmeoff').checked;
-    var offlineInputs = document.querySelectorAll('.pix_payment_method_select');
+    var offlineCheck = document.getElementById("checkmeoff").checked;
+    var offlineInputs = document.querySelectorAll(".pix_payment_method_select");
     for (var i = 0; i < offlineInputs.length; i++) {
       if (offlineCheck === true) {
         offlineInputs[i].checked = true;
@@ -116,36 +113,45 @@ const payment_mercado_pago_ticket = {
   },
   createOfflinePaymentsCheckAll: function () {
     //offline payments configuration form
-    var offline_payment_translate = '';
-    var offlineChecked = '';
+    var offline_payment_translate = "";
+    var offlineChecked = "";
     var countOfflineChecked = 0;
-    var offlineInputs = document.querySelectorAll('.pix_payment_method_select');
+    var offlineInputs = document.querySelectorAll(".pix_payment_method_select");
     for (var ioff = 0; ioff < offlineInputs.length; ioff++) {
-      offline_payment_translate = offlineInputs[ioff].getAttribute('data-translate');
+      offline_payment_translate =
+        offlineInputs[ioff].getAttribute("data-translate");
       if (offlineInputs[ioff].checked === true) {
         countOfflineChecked += 1;
       }
     }
 
     if (countOfflineChecked === offlineInputs.length) {
-      offlineChecked = 'checked';
+      offlineChecked = "checked";
     }
 
     for (var offi = 0; offi < offlineInputs.length; offi++) {
       if (offi === 0) {
-        var checkbox_offline_prepend = '<div class="all_checkbox">\
+        var checkbox_offline_prepend =
+          '<div class="all_checkbox">\
           <label for="checkmeoff" id="mp_input_payments" style="margin-bottom: 37px !important;">\
-            <input type="checkbox" name="checkmeoff" id="checkmeoff" ' + offlineChecked + ' onclick="this.completeOfflineCheckbox()">\
-            ' + offline_payment_translate + '\
+            <input type="checkbox" name="checkmeoff" id="checkmeoff" ' +
+          offlineChecked +
+          ' onclick="this.completeOfflineCheckbox()">\
+            ' +
+          offline_payment_translate +
+          "\
           </label>\
-        </div>';
-        offlineInputs[offi].parentElement.insertAdjacentHTML('beforebegin', checkbox_offline_prepend);
+        </div>";
+        offlineInputs[offi].parentElement.insertAdjacentHTML(
+          "beforebegin",
+          checkbox_offline_prepend
+        );
         break;
       }
     }
-  }
+  },
 };
 
-window.addEventListener('load', function () {
+window.addEventListener("load", function () {
   payment_mercado_pago_ticket.initScreen();
 });
--- includes/payments/hooks/class-wc-woomercadopago-hook-abstract.php
+++ includes/payments/hooks/class-wc-woomercadopago-hook-abstract.php
@@ -267,8 +267,6 @@ abstract class WC_WooMercadoPago_Hook_Abstract {
 			}
 		}
 
-		$this->update_others_checkout_mode($this->payment->id);
-
 		WC_WooMercadoPago_Helpers_CurrencyConverter::get_instance()->schedule_notice(
 			$this->payment,
 			$old_data,
@@ -348,37 +346,6 @@ abstract class WC_WooMercadoPago_Hook_Abstract {
 	 */
 	private function build_woocommerce_settings_key( $gateway_id ) {
 		return 'woocommerce_' . $gateway_id . '_settings';
-	}
-
-	/**
-	 * Update others checkout mode
-	 *
-	 * @param String $current_gateway_id Current constant ID
-	 *
-	 * @return void
-	 */
-	private function update_others_checkout_mode( $current_gateway_id ) {
-		foreach ( WC_WooMercadoPago_Constants::GATEWAYS_IDS as $gateway_id ) {
-			$gateway_settings_key = $this->build_woocommerce_settings_key($gateway_id);
-			$options              = get_option( $gateway_settings_key );
-			$is_other_gateway     = $gateway_id !== $current_gateway_id;
-
-			if ( empty($options) || ! array_key_exists('enabled', $options) ) {
-				continue;
-			}
-
-			$is_enabled_gateway = 'yes' === $options['enabled'];
-
-			if ( $is_other_gateway && $is_enabled_gateway ) {
-				$options['checkbox_checkout_test_mode']       = $this->payment->settings['checkbox_checkout_test_mode'];
-				$options['checkbox_checkout_production_mode'] = $this->payment->settings['checkbox_checkout_production_mode'];
-
-				update_option(
-					$gateway_settings_key,
-					apply_filters( 'woocommerce_settings_api_sanitized_fields_' . $gateway_id, $options )
-				);
-			}
-		}
 	}
 
 	/**
