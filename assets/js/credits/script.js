241796310e011abac7aef425ed38ad6521b320af
cleitonaguiarandrade <cleiton.andrade@mercadolivre.com>
2022-06-20 10:40:13 -0300

    Fixed html structure and added modal bottom to top slide  function for moblie devices

--- assets/js/credits/script.js
+++ assets/js/credits/script.js
@@ -1,15 +1,36 @@
-const switchModal = () => {
-  const modal = document.querySelector('#mp-credits-modal')
-  const actualStyle = modal.style.display
+(() => {
+  const switchModal = () => {
+    const modal = document.querySelector('#mp-credits-modal')
+    const actualStyle = modal.style.display
+    const modalContent = document.querySelector('.mp-credits-modal-container-content')
 
-  if (actualStyle == 'block') {
-    modal.style.display = 'none'
-  } else {
-    modal.style.display = 'block'
+
+    if (actualStyle == 'block') {
+      modal.style.display = 'none'
+      modalContent.classList.remove('mp-mobile')
+    }
+    else {
+      modal.style.display = 'block'
+      if (modal.clientWidth < 666) {
+        const modalHeight = modal.clientHeight
+        const modalContentHeight = modalContent.clientHeight
+        let top = modalHeight - modalContentHeight
+        modalContent.style.top = `${top}.px`
+        modalContent.classList.add('mp-mobile')
+      }
+    }
   }
-}
 
-const openBtn = document.querySelector('#mp-open-modal')
-openBtn.addEventListener('click', switchModal)
-const closebBtn = document.querySelector('#mp-credits-modal-close-modal')
-closebBtn.addEventListener('click', switchModal)
+  const openBtn = document.querySelector('#mp-open-modal')
+  openBtn.addEventListener('click', switchModal)
+  const closebBtn = document.querySelector('#mp-credits-modal-close-modal')
+  closebBtn.addEventListener('click', switchModal)
+
+  window.onclick = function (event) {
+    console.log(event)
+    const modal = document.querySelector('.mp-credits-modal-container')
+    if (event.target == modal) {
+      switchModal()
+    }
+  }
+})()
