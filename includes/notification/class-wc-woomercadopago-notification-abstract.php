03d99ff00b7678e50975be605f71f44ab9a4506c
DouglasCorreiaMeli <douglas.correia@mercadolivre.com>
2022-07-27 11:15:19 -0300

    fix log

--- includes/notification/class-wc-woomercadopago-notification-abstract.php
+++ includes/notification/class-wc-woomercadopago-notification-abstract.php
@@ -441,9 +441,9 @@ abstract class WC_WooMercadoPago_Notification_Abstract {
 	 * @return bool
 	 */
 	protected function can_update_order_status( $order ) {
-		$log = WC_WooMercadoPago_Log::init_mercado_pago_log( 'Notification_Partial' );
-        $log->write_log('mercadopago_notification_partial', 'Order Details' . $order);
-        $log->write_log('mercadopago_notification_partial', 'Partial Refund' . method_exists($order, 'get_status_detail'));
+        $this->log->write_log( __FUNCTION__, 'Order Details' . $order);
+        $this->log->write_log( __FUNCTION__, 'Order Status Detail' . method_exists($order, 'get_status_detail'));
+
         if ($order->get_status() == 'approved' && $order->get_status_detail() == 'partially_refunded') {
             return false;
         }
