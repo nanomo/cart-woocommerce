1dd14c76262d5a9a85d3de87fd9af80799672aa5
wiloliveira <willis.oliveira@mercadopago.com>
2022-08-17 12:25:55 -0300

    remove Whitespace

--- includes/module/mercadopago-settings/class-wc-woomercadopago-mercadopago-settings.php
+++ includes/module/mercadopago-settings/class-wc-woomercadopago-mercadopago-settings.php
@@ -522,7 +522,7 @@ class WC_WooMercadoPago_MercadoPago_Settings {
 
 			$verify_alert_test_mode = WC_WooMercadoPago_Credentials::get_sanitize_text_from_post( 'input_verify_alert_test_mode' );
 			$without_test_credentials = ( ( '' === get_option( WC_WooMercadoPago_Options::CREDENTIALS_PUBLIC_KEY_TEST, '' ) || '' === get_option( WC_WooMercadoPago_Options::CREDENTIALS_ACCESS_TOKEN_TEST, '' ) ) );
-			
+
 			if ( 'yes' === $verify_alert_test_mode || ( 'yes' === $checkout_test_mode && $without_test_credentials ) ) {
 				throw new Exception( __( 'Invalid credentials for test mode', 'woocommerce-mercadopago' ) );
 			} else {
